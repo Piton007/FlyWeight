7947a364095705b34e86844157c80f58b53a4d38
Jose <jose45321@outlook.com>
2020-05-29 13:10:42 -0500

    Add contact us functionality

--- js/main.js
+++ js/main.js
@@ -5,8 +5,67 @@
  });
 
 jQuery(document).ready(function($) {
-
 	"use strict";
+	let email = ""
+    let subject = ""
+    let message = ""
+
+    const sendBtnNode = document.getElementById("send")
+	const toastSuccess = $('#toast_success')
+	const toastDanger = $('#toast_danger')
+	toastSuccess.hide()
+	toastDanger.hide()
+
+	function setFieldValues(id,value){
+		switch (id) {
+		  case "email_address":
+			  email = value
+			break;
+		  case "email_subject":
+			  subject = value         
+			break;
+		  case "email_message":
+			  message = value
+			break;
+		
+		  default: console.log("error")
+			break;
+		}
+	  }
+
+	  
+
+	function sendEmail() {
+	  
+		if(email.length > 0 && subject.length > 0 && message.length > 0){
+			Email.send({
+				SecureToken : "cd6ff455-9bf1-4109-90f0-7e24530a9a64",
+				To : 'JoseMoWa@gmail.com',
+				From : email,
+				Subject : subject,
+				Body : message
+			  }).then(
+			  ()=>{
+				  toastSuccess.show()
+				  setTimeout(()=>{toastSuccess.hide()}, 2500);
+			  }
+			)
+			 }
+		else{
+			
+			toastDanger.show()
+			setTimeout(()=>{toastDanger.hide()}, 1000);
+		}
+    
+	}
+
+	const form  = document.getElementById("contact");
+    sendBtnNode.addEventListener("click",()=>{sendEmail()})
+    form.addEventListener("input",(event)=>{
+        setFieldValues(event.target.id,event.target.value)
+    })
+
+
 
 	$(".loader").delay(1000).fadeOut("slow");
   $("#overlayer").delay(1000).fadeOut("slow");	
